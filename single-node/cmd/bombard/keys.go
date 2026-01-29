6896e0b20e931cc04f12fe6758ca411e111a72a6
containerman17 <8990432+containerman17@users.noreply.github.com>
2026-01-29 12:41:51 +0000

    erc20

--- single-node/cmd/bombard/keys.go
+++ single-node/cmd/bombard/keys.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"math/big"
 
+	ethereum "github.com/ava-labs/libevm"
 	"github.com/ava-labs/libevm/common"
 	"github.com/ava-labs/libevm/core/types"
 	"github.com/ava-labs/libevm/crypto"
@@ -112,4 +113,101 @@ func weiToEther(wei *big.Int) float64 {
 	e := new(big.Float).SetInt(big.NewInt(1e18))
 	result, _ := new(big.Float).Quo(f, e).Float64()
 	return result
+}
+
+// ERC20 funding constants
+const (
+	erc20FundAmount    = 1000000 // Amount of tokens to fund each worker (in token units)
+	erc20FundThreshold = 500000  // Fund if balance is below this
+	erc20FundGasLimit  = 65000   // Gas limit for ERC20 transfer
+)
+
+// encodeBalanceOf returns calldata for balanceOf(address)
+func encodeBalanceOf(addr common.Address) []byte {
+	data := make([]byte, 36)
+	copy(data[0:4], []byte{0x70, 0xa0, 0x82, 0x31}) // balanceOf(address) selector
+	copy(data[16:36], addr.Bytes())                 // address padded to 32 bytes
+	return data
+}
+
+// encodeTransfer returns calldata for transfer(address,uint256)
+func encodeTransfer(to common.Address, amount *big.Int) []byte {
+	data := make([]byte, 68)
+	copy(data[0:4], []byte{0xa9, 0x05, 0x9c, 0xbb}) // transfer(address,uint256) selector
+	copy(data[16:36], to.Bytes())                   // address padded to 32 bytes
+	amount.FillBytes(data[36:68])                   // uint256
+	return data
+}
+
+// FundWorkersERC20 ensures each worker has ERC20 tokens for benchmarking.
+func FundWorkersERC20(
+	ctx context.Context,
+	client *ethclient.Client,
+	funderKey *ecdsa.PrivateKey,
+	signer types.Signer,
+	workerAddrs []common.Address,
+	tokenContract common.Address,
+) error {
+	funderAddr := crypto.PubkeyToAddress(funderKey.PublicKey)
+
+	// Check which workers need ERC20 funding
+	toFund := make([]int, 0)
+	threshold := new(big.Int).Mul(big.NewInt(erc20FundThreshold), big.NewInt(1e18))
+
+	for i, addr := range workerAddrs {
+		// Call balanceOf on the token contract
+		callData := encodeBalanceOf(addr)
+		result, err := client.CallContract(ctx, ethereum.CallMsg{
+			To:   &tokenContract,
+			Data: callData,
+		}, nil)
+		if err != nil {
+			return fmt.Errorf("failed to get ERC20 balance for worker %d: %w", i+1, err)
+		}
+
+		balance := new(big.Int).SetBytes(result)
+		if balance.Cmp(threshold) < 0 {
+			toFund = append(toFund, i)
+			fmt.Printf("Worker %d needs ERC20 funding (balance %.2f)\n", i+1, weiToEther(balance))
+		}
+	}
+
+	if len(toFund) == 0 {
+		fmt.Println("All workers have sufficient ERC20 balance")
+		return nil
+	}
+
+	// Get funder nonce
+	nonce, err := client.PendingNonceAt(ctx, funderAddr)
+	if err != nil {
+		return fmt.Errorf("failed to get funder nonce: %w", err)
+	}
+
+	// Fund workers that need it
+	amount := new(big.Int).Mul(big.NewInt(erc20FundAmount), big.NewInt(1e18))
+	gasPrice := big.NewInt(fundGasPrice)
+
+	for i, workerIdx := range toFund {
+		callData := encodeTransfer(workerAddrs[workerIdx], amount)
+		tx := types.NewTransaction(
+			nonce+uint64(i),
+			tokenContract,
+			big.NewInt(0),
+			erc20FundGasLimit,
+			gasPrice,
+			callData,
+		)
+		signed, err := types.SignTx(tx, signer, funderKey)
+		if err != nil {
+			return fmt.Errorf("failed to sign ERC20 funding tx for worker %d: %w", workerIdx+1, err)
+		}
+		err = client.SendTransaction(ctx, signed)
+		if err != nil {
+			return fmt.Errorf("failed to send ERC20 funding tx for worker %d: %w", workerIdx+1, err)
+		}
+		fmt.Printf("Funded worker %d with %d tokens\n", workerIdx+1, erc20FundAmount)
+	}
+
+	fmt.Printf("Sent %d ERC20 funding transactions\n", len(toFund))
+	return nil
 }
--- single-node/cmd/bombard/main.go
+++ single-node/cmd/bombard/main.go
@@ -28,9 +28,12 @@ const (
 	workerDelay = 50 * time.Millisecond
 	numWorkers  = int(tickerTime / workerDelay)
 
-	gasLimit = 21000
-	gasPrice = 25
+	gasLimitNative = 21000
+	gasLimitERC20  = 65000
+	gasPrice       = 25
 )
+
+var erc20Contract = common.HexToAddress("0xB0B5B0B5B0B5B0B5B0B5B0B5B0B5B0B5B0B5B0B5")
 
 var httpClient = &http.Client{
 	Transport: &http.Transport{
@@ -44,10 +47,11 @@ var httpClient = &http.Client{
 func main() {
 	rpcURL := flag.String("rpc", "", "RPC URL")
 	targetTps := flag.Int("tps", defaultTps, "Target transactions per second")
+	erc20Mode := flag.Bool("erc20", false, "Send ERC20 transfers instead of native transfers")
 	flag.Parse()
 
 	if *rpcURL == "" {
-		fmt.Println("Usage: bombard --rpc=<url> [--tps=4000]")
+		fmt.Println("Usage: bombard --rpc=<url> [--tps=4000] [--erc20]")
 		os.Exit(1)
 	}
 
@@ -99,6 +103,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Fund workers with ERC20 tokens if in ERC20 mode
+	if *erc20Mode {
+		fmt.Println("Checking worker ERC20 balances...")
+		err = FundWorkersERC20(ctx, client, privateKey, signer, workerAddrs, erc20Contract)
+		if err != nil {
+			fmt.Printf("Failed to fund workers with ERC20: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
 	// Wait for funding txs to be mined
 	fmt.Println("Waiting for funding transactions...")
 	time.Sleep(3 * time.Second)
@@ -106,12 +120,16 @@ func main() {
 	// Start block watcher
 	go watchBlocks(ctx, rpcClient)
 
-	fmt.Printf("\nStarting %d workers: send %d txs every %v, staggered by %v\n\n", numWorkers, batchSize, tickerTime, workerDelay)
+	mode := "native"
+	if *erc20Mode {
+		mode = "ERC20"
+	}
+	fmt.Printf("\nStarting %d workers (%s): send %d txs every %v, staggered by %v\n\n", numWorkers, mode, batchSize, tickerTime, workerDelay)
 
 	// Start workers with staggered delays
 	for i := 0; i < numWorkers; i++ {
 		workerID := i + 1
-		go runWorker(ctx, client, workerKeys[i], signer, workerAddrs[i], workerID, batchSize)
+		go runWorker(ctx, client, workerKeys[i], signer, workerAddrs[i], workerID, batchSize, *erc20Mode)
 		if i < numWorkers-1 {
 			time.Sleep(workerDelay)
 		}
@@ -129,6 +147,7 @@ func runWorker(
 	address common.Address,
 	workerID int,
 	batchSize int,
+	erc20 bool,
 ) {
 	ticker := time.NewTicker(tickerTime)
 	defer ticker.Stop()
@@ -136,14 +155,14 @@ func runWorker(
 	round := 0
 
 	// Run immediately on start
-	runWorkerRound(ctx, client, privateKey, signer, address, workerID, &round, batchSize)
+	runWorkerRound(ctx, client, privateKey, signer, address, workerID, &round, batchSize, erc20)
 
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case <-ticker.C:
-			runWorkerRound(ctx, client, privateKey, signer, address, workerID, &round, batchSize)
+			runWorkerRound(ctx, client, privateKey, signer, address, workerID, &round, batchSize, erc20)
 		}
 	}
 }
@@ -157,6 +176,7 @@ func runWorkerRound(
 	workerID int,
 	round *int,
 	batchSize int,
+	erc20 bool,
 ) {
 	*round++
 
@@ -168,10 +188,19 @@ func runWorkerRound(
 	}
 
 	// Send batch (to self)
-	_, errors := sendBatch(ctx, client, privateKey, signer, address, address, nonce, batchSize)
+	_, errors := sendBatch(ctx, client, privateKey, signer, address, address, nonce, batchSize, erc20)
 	if errors > 0 {
 		fmt.Printf("[Worker %d] Errors: %d\n", workerID, errors)
 	}
+}
+
+// encodeERC20Transfer returns calldata for transfer(address,uint256)
+func encodeERC20Transfer(to common.Address, amount *big.Int) []byte {
+	data := make([]byte, 68)
+	copy(data[0:4], []byte{0xa9, 0x05, 0x9c, 0xbb}) // transfer(address,uint256) selector
+	copy(data[16:36], to.Bytes())                   // address padded to 32 bytes
+	amount.FillBytes(data[36:68])                   // uint256
+	return data
 }
 
 func sendBatch(
@@ -182,6 +211,7 @@ func sendBatch(
 	from, to common.Address,
 	startNonce uint64,
 	count int,
+	erc20 bool,
 ) (sent, errors int) {
 	for i := 0; i < count; i++ {
 		select {
@@ -190,14 +220,27 @@ func sendBatch(
 		default:
 		}
 
-		tx := types.NewTransaction(
-			startNonce+uint64(i),
-			to,
-			big.NewInt(1), // 1 wei
-			gasLimit,
-			big.NewInt(gasPrice),
-			nil,
-		)
+		var tx *types.Transaction
+		if erc20 {
+			data := encodeERC20Transfer(to, big.NewInt(1))
+			tx = types.NewTransaction(
+				startNonce+uint64(i),
+				erc20Contract,
+				big.NewInt(0),
+				gasLimitERC20,
+				big.NewInt(gasPrice),
+				data,
+			)
+		} else {
+			tx = types.NewTransaction(
+				startNonce+uint64(i),
+				to,
+				big.NewInt(1), // 1 wei
+				gasLimitNative,
+				big.NewInt(gasPrice),
+				nil,
+			)
+		}
 
 		signed, err := types.SignTx(tx, signer, privateKey)
 		if err != nil {
